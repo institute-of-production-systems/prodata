5c43e75517bab0990627850ab049761f723c2e6d
Nikolai West <nikolai.west@tu-dortmund.de>
2025-06-27 08:23:27 +0200

    fix: add site data

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Projektstart im Forschungsprojekt ProData",
+        "excerpt":"Projektstart im Forschungsprojekt ProData Datenkompetenzen und Maschinelles Lernen zur prozessübergreifenden Qualitätssicherung in der Materialforschung Die Qualität spritzgegossener Bauteile nimmt in der Materialforschung aufgrund erweiterter Einsatzgebiete und höherer mechanischer Belastungen zunehmend an Bedeutung zu. In diesem Monat ist das Forschungsprojekt „ProData” gestartet, welches das Ziel verfolgt, die Qualitätssicherung und Prozessoptimierung in...","categories": ["Forschung"],
+        "tags": ["Projektstart","Machine Learning","Materialforschung","Spritzguss","Qualitätssicherung"],
+        "url": "/forschung/project-gestarted/",
+        "teaser": null
+      },{
+        "title": "Workshop zum Einsatz von Maschinellem Lernen in Kassel durchgeführt",
+        "excerpt":"Workshop zum Einsatz von Maschinellem Lernen in Kassel durchgeführt Ein wichtiger Bestandteil von ProData ist es die Datenkompetenzen beim domänenbezogenen Partner IfW zu fördern. Dazu fand im Juli 2023 ein gemeinsamer Workshop in Kassel statt, der sich den „Grundlagen des Maschinellen Lernens” widmete. 🤖 Was ist Maschinelles Lernen? Maschinelles Lernen...","categories": ["Veranstaltungen"],
+        "tags": ["Workshop","Maschinelles Lernen","RapidMiner","Datenkompetenzen","Kassel"],
+        "url": "/veranstaltungen/kassel-machine-learning/",
+        "teaser": null
+      },{
+        "title": "Unsupervised Anomaly Detection in Unbalanced Time Series Data from Screw Driving Processes using k-means Clustering",
+        "excerpt":"Ansatz zur unüberwachten Erkennung von Anomalien erschienen Da Schraubverbindungen in der Fertigung allgegenwärtig sind, ist ihre effektive und zuverlässige Qualitätssicherung besonders wichtig. Wir stellen einen clusterbasierten Ansatz zur unüberwachten Erkennung von fehlerhaften Schraubvorgängen vor. 🔩 Herausforderung: Qualitätssicherung bei Schraubverbindungen Die meisten Anziehverfahren stützen sich auf statistische Methoden, um fehlerhafte Schraubverbindungen...","categories": ["news","publications"],
+        "tags": ["Anomaly Detection","Time Series Clustering","Unsupervised Learning","Screw Driving","Machine Learning","Manufacturing","K-Means","Dynamic Time Warping"],
+        "url": "/news/publications/paper-cirp-cms-23/",
+        "teaser": "/assets/images/clustering-teaser.jpg"
+      },{
+        "title": "Ansatz zur unüberwachten Erkennung von Anomalien erschienen",
+        "excerpt":"Ansatz zur unüberwachten Erkennung von Anomalien erschienen 🔩 Herausforderung: Qualitätssicherung bei Schraubverbindungen Da Schraubverbindungen in der Fertigung allgegenwärtig sind, ist ihre effektive und zuverlässige Qualitätssicherung besonders wichtig. Die meisten Anziehverfahren stützen sich auf statistische Methoden, um fehlerhafte Schraubverbindungen bereits während der Montage zu erkennen. 🧠 Unser innovativer Ansatz In diesem...","categories": ["Veröffentlichungen"],
+        "tags": ["Forschung","Anomalieerkennung","Machine Learning","Schraubverbindungen","Clustering","K-Means"],
+        "url": "/ver%C3%B6ffentlichungen/ueberwachte-erkennung/",
+        "teaser": null
+      },{
+        "title": "Vertiefender Workshop zum Arbeiten mit RapidMiner durchgeführt",
+        "excerpt":"Vertiefender Workshop zum Arbeiten mit RapidMiner durchgeführt In einem vorherigen Workshop zum Einsatz von Maschinellem Lernen im Juli 2023 wurden zum einen die Grundlagen des maschinellen Lernens und zum anderen eine Einführung in die Nutzung der Datenanalysesoftware RapidMiner behandelt. Aufbauend auf diesen Workshop wurde im Herbst 2023 auf Wunsch der...","categories": ["Veranstaltungen"],
+        "tags": ["Workshop","RapidMiner","Machine Learning","Clustering","Klassifikation","Datenanalyse"],
+        "url": "/veranstaltungen/rapidminer-workshop/",
+        "teaser": null
+      },{
+        "title": "Comparative Study of Machine Learning Methods for Anomaly Detection in Screwing Data",
+        "excerpt":"Systematischer Vergleich von ML-Methoden für Schraubdaten Diese Publikation präsentiert eine umfassende Vergleichsstudie verschiedener Machine Learning Ansätze zur Anomalieerkennung in Schraubprozessen und adressiert damit ein zentrales Problem der modernen Qualitätssicherung. 🔧 Industrieller Kontext Herausforderung der Schraubverbindungen: Aufgrund ihrer Häufigkeit in der Fertigung ist die effektive und zuverlässige Qualitätssicherung bei Schraubverbindungen besonders...","categories": ["news","publications"],
+        "tags": ["Machine Learning","Anomaly Detection","Screwing Process","Manufacturing"],
+        "url": "/news/publications/paper-hicss-24/",
+        "teaser": "/assets/images/ml-screwing-teaser.jpg"
+      }]
